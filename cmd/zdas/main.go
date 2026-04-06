998235f79e25b60f6f9685bd2db045735fd5ca17
Dave Hart <dphart@rochester.rr.com>
2026-04-06 10:37:54 -0400

    Wire up main entrypoint with config and signal handling

--- cmd/zdas/main.go
+++ cmd/zdas/main.go
@@ -1,5 +1,54 @@
 // Command zdas runs the Ziti Device Authentication Service as a standalone binary.
 package main
 
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/smilindave26/zdas/pkg/zdas"
+)
+
 func main() {
+	configPath := flag.String("config", "", "path to config file (YAML)")
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+		Level: slog.LevelInfo,
+	}))
+
+	cfg, err := zdas.LoadConfig(*configPath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "config error: %v\n", err)
+		os.Exit(1)
+	}
+
+	srv, err := zdas.NewServer(*cfg, logger)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "server setup error: %v\n", err)
+		os.Exit(1)
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	go func() {
+		<-ctx.Done()
+		logger.Info("shutting down")
+		shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		if err := srv.Shutdown(shutdownCtx); err != nil {
+			logger.Error("shutdown error", "error", err)
+		}
+	}()
+
+	if err := srv.Start(ctx); err != nil {
+		logger.Error("server exited", "error", err)
+		os.Exit(1)
+	}
 }
